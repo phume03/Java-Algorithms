a01a7e84a1b27dae78daacc596d5feeac19ff60e
Phumelela Mdluli <96667636+phume03@users.noreply.github.com>
2025-06-16 22:00:34 +0000

    first commit from codespaces - end

--- Algorithms_Introduction.java
+++ Algorithms_Introduction.java
@@ -1,4 +1,4 @@
-public class Algorithms {
+public class Algorithms_Introduction {
 
     public static int findMaximum(int a, int b, int c) {
         if (a > b) {
@@ -14,12 +14,34 @@ public class Algorithms {
         return c;
     }
 
+    public static int findMaximum2(int a, int b, int c) {
+        int max = a;
+
+        if (b > max) {
+            max = b;
+        }
+
+        if (c > max) {
+            max = c;
+        }
+
+        return max;
+    }
+
     public static void main(String[] args) {
 
+        System.out.println("Find Maximum: ");
         System.out.println(findMaximum(1, 2, 3));
         System.out.println(findMaximum(8, 8, 1));
         System.out.println(findMaximum(3, 2, 3));
         System.out.println(findMaximum(1, 1, 9));
         System.out.println(findMaximum(1, 9, 9));
+        System.out.println();
+        System.out.println("Find Maximum 2: ");
+        System.out.println(findMaximum2(1, 2, 3));
+        System.out.println(findMaximum2(8, 8, 1));
+        System.out.println(findMaximum2(3, 2, 3));
+        System.out.println(findMaximum2(1, 1, 9));
+        System.out.println(findMaximum2(1, 9, 9));
     }
 }
