f89406ba393880edcabb9ac39d3b72aaa63e62c3
Phumelela Mdluli <96667636+phume03@users.noreply.github.com>
2025-06-16 22:20:05 +0000

    01 04 - end

--- Algorithms_04.java
+++ Algorithms_04.java
@@ -1,6 +1,26 @@
 public class Algorithms_04 {
 
+    public static void parseContents(String s) {
+        System.out.println("Parse Contents: "+s);
+        System.out.println("Option 1");
+        for (char c : s.toCharArray()) {
+            System.out.print(c+" -> ");
+        }
+
+        System.out.println();
+        System.out.println("Option 2");
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            System.out.print(c+" -> ");
+        }
+        System.out.println();
+
+    }
+
     public static void main(String[] args) {
         String s = "HeLLo";
+        System.out.println(s+" contains LL:"+s.contains("LL"));
+
+        parseContents(s);
     }
 }
