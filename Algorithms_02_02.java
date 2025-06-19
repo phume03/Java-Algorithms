455053e75ed16c05663bf12d4c22bc282e102347
Phumelela Mdluli <96667636+phume03@users.noreply.github.com>
2025-06-19 03:29:28 +0000

    02 02 - end

--- Algorithms_02_02.java
+++ Algorithms_02_02.java
@@ -1,3 +1,29 @@
-public class Algorithms_02_02 {
+import java.util.Arrays;
+import java.util.OptionalInt;
 
+public class Algorithms_02_02 {
+    public static Integer linearSearch(int[] arr, int item) {
+        for (int current : arr) {
+            if (current == item) {
+                return item;
+            }
+        }
+
+        return null;
+    }
+
+    public static OptionalInt linearSearch2(int[] arr, int item) {
+        return Arrays.stream(arr).filter(x -> x == item).findFirst();
+    }  
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        System.out.println("Array: {1, 2, 3, 4, 5, 6}"); // inefficient print, duplication of array code, if array changes, this will also have to change and can be missed
+        System.out.println("Find 1 in array: "+linearSearch(arr, 1));
+        System.out.println("Find 8 in array: "+linearSearch(arr, 8));
+        System.out.println();
+        linearSearch2(arr, 1).ifPresent(x -> System.out.println("Find 1 in array: "+x));
+        linearSearch2(arr, 8).ifPresent(x -> System.out.println("Find 8 in array: "+x));      
+        
+    }
 }
