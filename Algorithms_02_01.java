f7b4f3c65b7d5650d711ebed8221d9062dd3e7a2
Phumelela Mdluli <96667636+phume03@users.noreply.github.com>
2025-06-19 03:08:56 +0000

    02 01 - end

--- Algorithms_02_01.java
+++ Algorithms_02_01.java
@@ -1,3 +1,13 @@
 public class Algorithms_02_01 {
+    public static Integer linearSearch(int[] arr, int item) {
+        for (int data: arr) if (data == item) return new Integer(data);
 
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        System.out.println(linearSearch(arr, 1));
+        System.out.println(linearSearch(arr, 8));
+    }
 }
