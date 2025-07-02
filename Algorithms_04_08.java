7c649c3222db41042dae3252b9ee916b4d720a67
Phumelela Mdluli <96667636+phume03@users.noreply.github.com>
2025-07-02 01:27:59 +0000

    04 08 - end

--- Algorithms_04_08.java
+++ Algorithms_04_08.java
@@ -1,22 +1,37 @@
 public class Algorithms_04_08 {
 
     public static boolean hasMatchingParentheses(String s) {
-        return false;
+        int matchingSymbolTracker = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            if (current == '(') {
+                matchingSymbolTracker++;
+            } else if (current == ')') {
+                if (matchingSymbolTracker > 0) {
+                    matchingSymbolTracker--;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return matchingSymbolTracker == 0;
     }
 
     public static void main(String[] args) {
-        System.out.println(hasMatchingParentheses("((algorithm()))"));
-        System.out.println(hasMatchingParentheses("()(algorithm())"));
-        System.out.println(hasMatchingParentheses("((algorithm))"));
-        System.out.println(hasMatchingParentheses("(algorithm)"));
+        System.out.println("((algorithm())) has matching parentheses: "+hasMatchingParentheses("((algorithm()))"));
+        System.out.println("()(algorithm()) has matching parentheses: "+hasMatchingParentheses("()(algorithm())"));
+        System.out.println("((algorithm)) has matching parentheses: "+hasMatchingParentheses("((algorithm))"));
+        System.out.println("(algorithm) has matching parentheses: "+hasMatchingParentheses("(algorithm)"));
 
         System.out.println();
 
-        System.out.println(hasMatchingParentheses("(algorithm("));
-        System.out.println(hasMatchingParentheses(")algorithm)"));
-        System.out.println(hasMatchingParentheses(")algorithm("));
-        System.out.println(hasMatchingParentheses("algorithm(("));
-        System.out.println(hasMatchingParentheses("(algorithm"));
-        System.out.println(hasMatchingParentheses("((algorithm)"));
+        System.out.println("(algorithm( has matching parentheses: "+hasMatchingParentheses("(algorithm("));
+        System.out.println(")algorithm) has matching parentheses: "+hasMatchingParentheses(")algorithm)"));
+        System.out.println(")algorithm( has matching parentheses: "+hasMatchingParentheses(")algorithm("));
+        System.out.println("algorithm(( has matching parentheses: "+hasMatchingParentheses("algorithm(("));
+        System.out.println("(algorithm has matching parentheses: "+hasMatchingParentheses("(algorithm"));
+        System.out.println("((algorithm) has matching parentheses: "+hasMatchingParentheses("((algorithm)"));
     }
 }
