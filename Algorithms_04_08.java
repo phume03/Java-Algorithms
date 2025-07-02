87e68ed0914d02ed65758e556f392ad7c02983a3
Phumelela Mdluli <96667636+phume03@users.noreply.github.com>
2025-07-02 01:21:38 +0000

    04 08 - start

---
+++ Algorithms_04_08.java
@@ -0,0 +1,22 @@
+public class Algorithms_04_08 {
+
+    public static boolean hasMatchingParentheses(String s) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasMatchingParentheses("((algorithm()))"));
+        System.out.println(hasMatchingParentheses("()(algorithm())"));
+        System.out.println(hasMatchingParentheses("((algorithm))"));
+        System.out.println(hasMatchingParentheses("(algorithm)"));
+
+        System.out.println();
+
+        System.out.println(hasMatchingParentheses("(algorithm("));
+        System.out.println(hasMatchingParentheses(")algorithm)"));
+        System.out.println(hasMatchingParentheses(")algorithm("));
+        System.out.println(hasMatchingParentheses("algorithm(("));
+        System.out.println(hasMatchingParentheses("(algorithm"));
+        System.out.println(hasMatchingParentheses("((algorithm)"));
+    }
+}
